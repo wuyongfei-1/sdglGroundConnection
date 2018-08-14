f93362acca8f86b8c4c37edcb71b84ffb4f63fcf
wtyh52java <wtyh52java@163.com>
2018-08-14 11:19:08 +0800

    wangtao pojo commit

--- src/main/java/com/dyhc/sdglgroundconnection/pojo/AccountType.java
+++ src/main/java/com/dyhc/sdglgroundconnection/pojo/AccountType.java
@@ -1,33 +1,38 @@
 package com.dyhc.sdglgroundconnection.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
 import java.util.Date;
 import java.util.Objects;
 
 /**
  * this class by created wuyongfei on 2018/6/5 13:50
+ * 账户类型
  **/
 @Table(name = "accounttype")
 public class AccountType {
     @Id
     @Column(name = "accountTypeId")
-    private Integer accountTypeId;
-    private String bank;
+    private Integer accountTypeId; // 账户类型表编号
+    private String bank; // 开户行
     @Column(name = "accountNum")
-    private String accountNum;
+    private String accountNum; // 账号/卡号
     @Column(name = "accountName")
-    private String accountName;
-    private Integer status;
+    private String accountName; // 账户名/户名
+    private Integer status; // 状态  （1.个人   2.企业）
     @Column(name = "whetherDel")
-    private Integer whetherDel;
+    private Integer whetherDel; // 是否删除（1代表已删除，0代表未删除）
     @Column(name = "createBy")
-    private Integer createBy;
+    private Integer createBy; // 创建人 （外键，与人员表关联
     @Column(name = "updateBy")
-    private Integer updateBy;
+    private Integer updateBy; // 修改人（外键，与人员表关联）
+    @JsonFormat(pattern = "yyyy-MM-dd")
     @Column(name = "upDate")
-    private Date upDate;
+    private Date upDate; // 修改日期
+    @JsonFormat(pattern = "yyyy-MM-dd")
     @Column(name = "createDate")
-    private Date createDate;
+    private Date createDate; // 创建时间
     private String value1;
     private String value2;
     private String value3;

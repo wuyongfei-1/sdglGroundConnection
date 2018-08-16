682381ee0d4ce59270ba43caedd73bcad26a687f
ygh-2 <yghajava@163.com>
2018-08-16 18:06:06 +0800

    yunguohao commit

--- src/main/java/com/dyhc/sdglgroundconnection/service/TravelService.java
+++ src/main/java/com/dyhc/sdglgroundconnection/service/TravelService.java
@@ -1,5 +1,7 @@
 package com.dyhc.sdglgroundconnection.service;
 
+import com.dyhc.sdglgroundconnection.pojo.Travel;
+import com.github.pagehelper.PageInfo;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -9,4 +11,31 @@ import org.springframework.stereotype.Service;
  * 组团社业务接口
  **/
 public interface TravelService {
+    /**
+     * 组团社名字查询（yunguohao）
+     * @param pageNo
+     * @param PageSize
+     * @return
+     * @throws Exception
+     */
+    PageInfo<Travel> listCompany(Integer pageNo, Integer PageSize, Travel travel) throws Exception;
+    /**
+     * 组团社增加（yunguohao）
+     */
+    int insertTravels(Travel travel);
+    /**
+     * 组团社修改（yunguohao）
+     */
+    int updateTravels(Travel travel);
+    /**
+     * 组团社删除（yunguohao）
+     */
+    int deleteTravelByIDs(int travelid) throws  Exception;
+
+    /**
+     * 组团社id查询（yunguohao）
+     * @param id
+     * @return
+     */
+    Travel selectTravelByIds(int id);
 }

644d4e6cc5b4fd8c35d05d74d34dc3236552edd5
yeshenye <632050263@qq.com>
2018-08-17 14:30:08 +0800

    dubingkun commit

--- src/main/java/com/dyhc/sdglgroundconnection/web/EnterCountAdjustPageController.java
+++ src/main/java/com/dyhc/sdglgroundconnection/web/EnterCountAdjustPageController.java
@@ -1,6 +1,8 @@
 package com.dyhc.sdglgroundconnection.web;
 
+import com.dyhc.sdglgroundconnection.pojo.Hotel;
 import com.dyhc.sdglgroundconnection.pojo.RoomType;
+import com.dyhc.sdglgroundconnection.service.HotelService;
 import com.dyhc.sdglgroundconnection.service.RoomTypeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +24,8 @@ import javax.servlet.http.HttpServletRequest;
 public class EnterCountAdjustPageController {
     @Autowired
     private RoomTypeService roomTypeService;
+    @Autowired
+    private HotelService hotelService;
 
     // 日志对象
     private Logger logger = LoggerFactory.getLogger(EnterGeneralControlPageController.class);
@@ -135,7 +139,19 @@ public class EnterCountAdjustPageController {
      * @return
      */
     @RequestMapping("/hotel-add.html")
-    public String  hoteladd() {
+    public String  hoteladd(HttpServletRequest request,Integer pan,Integer hotelId) {
+        Hotel hotel=hotelService.selectHotelById(hotelId);
+        //修改
+        if(pan!=0){
+            try {
+                hotelId=hotel.getHotelId();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        request.setAttribute("ht",hotel);
+        request.setAttribute("pan",pan);
+        request.setAttribute("hotelId",hotelId);
         return "countAdjust/index/hotel-add";
     }
     /**

82564c7b5553af821ea3c955b7b1ab40edffca38
wuyongfei-1 <1402218191@qq.com>
2018-08-17 20:46:04 +0800

    wuyongfei commit

--- src/main/java/com/dyhc/sdglgroundconnection/web/DictionariesController.java
+++ src/main/java/com/dyhc/sdglgroundconnection/web/DictionariesController.java
@@ -10,7 +10,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -31,14 +33,35 @@ public class DictionariesController {
 
 
     /**
-     * 获取所有和景点相关的字典信息  （wangtao）
+     * 根据类型编码获取所有的字典信息（wuyongfei）
+     *
+     * @param typeCode 类型编码
+     * @return 响应结果
+     */
+    @GetMapping(value = "/dictionaries/all")
+    public ReponseResult showAllMealTypes(@RequestParam("typeCode") String typeCode) {
+        try {
+            List<Dictionaries> dictionaries = dictionariesService.listDictionaries(typeCode);
+            logger.info(" method:showAllMealTypes  获取对应的所有字典成功！");
+            return ReponseResult.ok(dictionaries, "获取对应的所有字典成功！");
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error(" method:showAllMealTypes  获取对应的所有字典失败！");
+            return ReponseResult.err("获取对应的所有字典成功，系统出现异常！");
+        }
+    }
+
+
+    /**
+     * 获取所有和景点相关的字典信息  （wangtao）（获取分页后的数据）
+     *
      * @return 返回ReponseResult对象
      */
     @RequestMapping("/ListDictionariesByScenicspot")
-    public ReponseResult ListDictionariesByScenicspot(){
+    public ReponseResult ListDictionariesByScenicspot() {
         try {
             //一、查询所有的景点相关字典信息
-            PageInfo<Dictionaries> pageInfo =dictionariesService.ListDictionariesByScenicepot("ATTRACTIONS");
+            PageInfo<Dictionaries> pageInfo = dictionariesService.ListDictionariesByScenicepot("ATTRACTIONS");
             //二、返回ReponseResult对象
             ReponseResult<List> data = ReponseResult.ok(pageInfo.getList(), pageInfo.getTotal(), "分页获取字典信息成功！");
             //三、录入日志并返回

58295cb50b9e7602459e97e041ee17125e0dbfee
ygh-2 <yghajava@163.com>
2018-08-24 17:30:18 +0800

    yunguohao commit

--- src/main/java/com/dyhc/sdglgroundconnection/pojo/Discar.java
+++ src/main/java/com/dyhc/sdglgroundconnection/pojo/Discar.java
@@ -24,13 +24,14 @@ public class Discar {
     @Column(name = "quotePrice")
     private Double quotePrice; // 报价
     private Integer number; // 数量
+    @Column(name = "`status`")
     private Integer status; // 是否删除（1代表已删除，0代表未删除）
     @Column(name = "createBy")
     private Integer createBy; // 创建人 （外键，与人员表关联）
     @Column(name = "updateBy")
     private Integer updateBy; // 修改人（外键，与人员表关联）
     @JsonFormat(pattern = "yyyy-MM-dd")
-    @Column(name = "upDate")
+    @Column(name = "`upDate`")
     private Date upDate; // 修改日期
     @JsonFormat(pattern = "yyyy-MM-dd")
     @Column(name = "createDate")

b0ac76a9ef4e16906a8186b8f6d2f65f26b9e810
wtyh52java <wtyh52java@163.com>
2018-08-24 17:52:16 +0800

    wangtao voucher commit

---
+++ src/main/java/com/dyhc/sdglgroundconnection/web/TourguideController.java
@@ -0,0 +1,47 @@
+package com.dyhc.sdglgroundconnection.web;
+
+import com.dyhc.sdglgroundconnection.pojo.Tourguide;
+import com.dyhc.sdglgroundconnection.service.TourguideService;
+import com.dyhc.sdglgroundconnection.utils.ReponseResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 导游带团日志 控制层 wangtao
+ */
+@Controller
+@RequestMapping("/tourguide")
+public class TourguideController {
+
+    // 日志对象
+    private Logger logger = LoggerFactory.getLogger(TemplateScenicspotController.class);
+    @Autowired
+    private TourguideService tourguideService;
+
+    /**
+     * 根据导游带团日志编号查询导游带团日志信息
+     * @param tourguideId 导游带团日志编号
+     * @return 返回导游带团日志对象
+     */
+    @RequestMapping("/getTourguideInfoByTourguideId")
+    public ReponseResult getTourguideInfoByTourguideId(@RequestParam("tourguideId")Integer tourguideId){
+        try {
+            //一、根据导游带团日志编号查询导游带团日志信息
+            Tourguide tourguide=tourguideService.getTourguideInfoByTourguideId(tourguideId);
+            //二、返回ReponseResult对象
+            ReponseResult<Tourguide> data = ReponseResult.ok(tourguide, "获取导游带团日志成功！");
+            //三、录入日志并返回
+            logger.info(" method:listAllInfo  获取导游带团日志成功！");
+            return data;
+        } catch (Exception e) {
+            logger.error(" method:listAllInfo  获取导游带团日志数据失败，系统出现异常！");
+            e.printStackTrace();
+            ReponseResult<Object> err = ReponseResult.err("系统出现异常！");
+            return err;
+        }
+    }
+}

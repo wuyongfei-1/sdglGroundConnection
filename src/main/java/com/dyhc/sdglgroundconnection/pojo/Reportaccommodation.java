038277b3f7193718e9e264f93b52b8577e291365
wtyh52java <wtyh52java@163.com>
2018-08-25 09:14:29 +0800

    wangtao voucher commit

--- src/main/java/com/dyhc/sdglgroundconnection/pojo/Reportaccommodation.java
+++ src/main/java/com/dyhc/sdglgroundconnection/pojo/Reportaccommodation.java
@@ -43,7 +43,7 @@ public class Reportaccommodation {
     @JsonFormat(pattern = "yyyy-MM-dd")
     @Column(name = "createDate")
     private Date createDate; // 创建时间
-    private String value1;
+    private String value1; // 导游报账明细表编号
     private String value2;
     private String value3;
 

413d258edf05fca16e61b2b92469fb3c5335c159
xj17635374061 <17635374061@163.com>
2018-08-27 20:59:34 +0800

    Merge branch 'master' of https://github.com/wuyongfei-1/sdglGroundConnection
    # Conflicts:
    #	src/main/java/com/dyhc/sdglgroundconnection/service/TourguideService.java
    #	src/main/java/com/dyhc/sdglgroundconnection/service/impl/TourguideServiceImpl.java

--- src/main/java/com/dyhc/sdglgroundconnection/service/impl/BillTypeServiceImpl.java
+++ src/main/java/com/dyhc/sdglgroundconnection/service/impl/BillTypeServiceImpl.java
@@ -1,9 +1,12 @@
 package com.dyhc.sdglgroundconnection.service.impl;
 
 import com.dyhc.sdglgroundconnection.mapper.BillTypeMapper;
+import com.dyhc.sdglgroundconnection.pojo.BillType;
 import com.dyhc.sdglgroundconnection.service.BillTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * this class by created wuyongfei on 2018/6/5 13:50
@@ -14,4 +17,13 @@ public class BillTypeServiceImpl implements BillTypeService {
 
     @Autowired
     private BillTypeMapper billTypeMapper;
+
+    /**
+     * 查询所有单据类型(lixiaojie)
+     * @return
+     */
+    @Override
+    public List<BillType> selectAllBillType() {
+        return billTypeMapper.selectAll();
+    }
 }

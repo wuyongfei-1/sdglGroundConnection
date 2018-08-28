d36764a2661d7561a89cef5de26880373c56ef12
xj17635374061 <17635374061@163.com>
2018-08-28 10:49:50 +0800

    Merge branch 'master' of https://github.com/wuyongfei-1/sdglGroundConnection
    # Conflicts:
    #	src/main/java/com/dyhc/sdglgroundconnection/service/TourguideService.java
    #	src/main/java/com/dyhc/sdglgroundconnection/service/impl/TourguideServiceImpl.java

--- src/main/java/com/dyhc/sdglgroundconnection/service/impl/DislineServiceImpl.java
+++ src/main/java/com/dyhc/sdglgroundconnection/service/impl/DislineServiceImpl.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class DislineServiceImpl implements DislineService {
 
     @Autowired
-    private DislineMapper dislineMapper;W
+    private DislineMapper dislineMapper;
 
     /**
      * 根据调度编号查询调度线路

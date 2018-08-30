923e51af70ccc9e53f9701780e670c7078151e2d
yeshenye <632050263@qq.com>
2018-08-30 17:10:20 +0800

    dubingkun commit

--- src/main/java/com/dyhc/sdglgroundconnection/web/TourguideController.java
+++ src/main/java/com/dyhc/sdglgroundconnection/web/TourguideController.java
@@ -45,29 +45,4 @@ public class TourguideController {
             return err;
         }
     }
-
-    /**
-     * 上传导游带团日志（dubingkun）
-     * @param tourguide
-     * @return
-     */
-    @RequestMapping(value = "upData")
-    public ReponseResult upData(@RequestBody Tourguide tourguide){
-        try {
-            ReponseResult<String> data;
-            int a=tourguideService.insertTourguide(tourguide);
-            if(a>0){
-                 data= ReponseResult.ok("上传成功", "上传导游带团日志成功！");
-            }else{
-                data= ReponseResult.ok("上传失败", "上传导游带团日志成功！");
-            }
-            logger.info(" method:getTourguideInfoByTourguideId  上传导游带团日志成功！");
-            return data;
-        } catch (Exception e) {
-            logger.error(" method:getTourguideInfoByTourguideId  上传导游带团日志数据失败，系统出现异常！");
-            e.printStackTrace();
-            ReponseResult<Object> err = ReponseResult.err("系统出现异常！");
-            return err;
-        }
-    }
 }

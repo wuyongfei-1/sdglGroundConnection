7d3dbdf5db76214b69f20cae48ec90dae1beab91
yeshenye <632050263@qq.com>
2018-08-30 17:01:46 +0800

    dubingkun commit

--- src/main/java/com/dyhc/sdglgroundconnection/web/TourguideController.java
+++ src/main/java/com/dyhc/sdglgroundconnection/web/TourguideController.java
@@ -6,6 +6,7 @@ import com.dyhc.sdglgroundconnection.utils.ReponseResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +40,31 @@ public class TourguideController {
             return data;
         } catch (Exception e) {
             logger.error(" method:getTourguideInfoByTourguideId  获取导游带团日志数据失败，系统出现异常！");
+            e.printStackTrace();
+            ReponseResult<Object> err = ReponseResult.err("系统出现异常！");
+            return err;
+        }
+    }
+
+    /**
+     * 上传导游带团日志（dubingkun）
+     * @param tourguide
+     * @return
+     */
+    @RequestMapping(value = "upData")
+    public ReponseResult upData(@RequestBody Tourguide tourguide){
+        try {
+            ReponseResult<String> data;
+            int a=tourguideService.insertTourguide(tourguide);
+            if(a>0){
+                 data= ReponseResult.ok("上传成功", "上传导游带团日志成功！");
+            }else{
+                data= ReponseResult.ok("上传失败", "上传导游带团日志成功！");
+            }
+            logger.info(" method:getTourguideInfoByTourguideId  上传导游带团日志成功！");
+            return data;
+        } catch (Exception e) {
+            logger.error(" method:getTourguideInfoByTourguideId  上传导游带团日志数据失败，系统出现异常！");
             e.printStackTrace();
             ReponseResult<Object> err = ReponseResult.err("系统出现异常！");
             return err;

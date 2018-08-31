cf7e0f2c90b5d4aded7ab9a5dd0d1739230a88d3
yeshenye <632050263@qq.com>
2018-08-31 08:31:38 +0800

    dubingkun commit

--- src/main/java/com/dyhc/sdglgroundconnection/wechat/GuideLogController.java
+++ src/main/java/com/dyhc/sdglgroundconnection/wechat/GuideLogController.java
@@ -1,7 +1,12 @@
 package com.dyhc.sdglgroundconnection.wechat;
 
+import com.dyhc.sdglgroundconnection.pojo.Tourguide;
+import com.dyhc.sdglgroundconnection.service.TourguideService;
+import com.dyhc.sdglgroundconnection.utils.ReponseResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +20,31 @@ public class GuideLogController {
 
     // 日志对象
     private Logger logger = LoggerFactory.getLogger(GuideLogController.class);
+    @Autowired
+    private TourguideService tourguideService;
+    /**
+     * 上传导游带团日志（dubingkun）
+     * @param tourguide
+     * @return
+     */
+    @RequestMapping(value = "upData")
+    public ReponseResult upData(@RequestBody Tourguide tourguide){
+        try {
+            ReponseResult<String> data;
+            int a=tourguideService.insertTourguide(tourguide);
+            if(a>0){
+                data= ReponseResult.ok("上传成功", "上传导游带团日志成功！");
+            }else{
+                data= ReponseResult.ok("上传失败", "上传导游带团日志成功！");
+            }
+            logger.info(" method:getTourguideInfoByTourguideId  上传导游带团日志成功！");
+            return data;
+        } catch (Exception e) {
+            logger.error(" method:getTourguideInfoByTourguideId  上传导游带团日志数据失败，系统出现异常！");
+            e.printStackTrace();
+            ReponseResult<Object> err = ReponseResult.err("系统出现异常！");
+            return err;
+        }
+    }
 
 }
--- src/main/java/com/dyhc/sdglgroundconnection/wechat/GuideUploadBillController.java
+++ src/main/java/com/dyhc/sdglgroundconnection/wechat/GuideUploadBillController.java
@@ -20,30 +20,5 @@ public class GuideUploadBillController {
 
     // 日志对象
     private Logger logger = LoggerFactory.getLogger(GuideUploadBillController.class);
-    @Autowired
-    private TourguideService tourguideService;
-    /**
-     * 上传导游带团日志（dubingkun）
-     * @param tourguide
-     * @return
-     */
-    @RequestMapping(value = "upData")
-    public ReponseResult upData(@RequestBody Tourguide tourguide){
-        try {
-            ReponseResult<String> data;
-            int a=tourguideService.insertTourguide(tourguide);
-            if(a>0){
-                data= ReponseResult.ok("上传成功", "上传导游带团日志成功！");
-            }else{
-                data= ReponseResult.ok("上传失败", "上传导游带团日志成功！");
-            }
-            logger.info(" method:getTourguideInfoByTourguideId  上传导游带团日志成功！");
-            return data;
-        } catch (Exception e) {
-            logger.error(" method:getTourguideInfoByTourguideId  上传导游带团日志数据失败，系统出现异常！");
-            e.printStackTrace();
-            ReponseResult<Object> err = ReponseResult.err("系统出现异常！");
-            return err;
-        }
-    }
+
 }

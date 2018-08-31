1b3cded49c9313fdfecbef67bf8706d777fcbd97
yeshenye <632050263@qq.com>
2018-08-31 10:24:25 +0800

    dubingkun commit

--- src/main/java/com/dyhc/sdglgroundconnection/wechat/GuideUploadBillController.java
+++ src/main/java/com/dyhc/sdglgroundconnection/wechat/GuideUploadBillController.java
@@ -70,30 +70,4 @@ public class GuideUploadBillController {
             return ReponseResult.err("上传失败！");
         }
     }
-    @Autowired
-    private TourguideService tourguideService;
-    /**
-     * 上传导游带团日志（dubingkun）
-     * @param tourguide
-     * @return
-     */
-    @RequestMapping(value = "upData")
-    public ReponseResult upData(@RequestBody Tourguide tourguide){
-        try {
-            ReponseResult<String> data;
-            int a=tourguideService.insertTourguide(tourguide);
-            if(a>0){
-                data= ReponseResult.ok("上传成功", "上传导游带团日志成功！");
-            }else{
-                data= ReponseResult.ok("上传失败", "上传导游带团日志成功！");
-            }
-            logger.info(" method:getTourguideInfoByTourguideId  上传导游带团日志成功！");
-            return data;
-        } catch (Exception e) {
-            logger.error(" method:getTourguideInfoByTourguideId  上传导游带团日志数据失败，系统出现异常！");
-            e.printStackTrace();
-            ReponseResult<Object> err = ReponseResult.err("系统出现异常！");
-            return err;
-        }
-    }
 }

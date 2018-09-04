1c0a283e08b45f308a88c4f57b0007687c3ee4a3
wuyongfei-1 <1402218191@qq.com>
2018-09-04 08:43:49 +0800

    wuyongfei commit

--- src/main/java/com/dyhc/sdglgroundconnection/SdglgroundconnectionApplication.java
+++ src/main/java/com/dyhc/sdglgroundconnection/SdglgroundconnectionApplication.java
@@ -2,6 +2,8 @@ package com.dyhc.sdglgroundconnection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 /*@MapperScan(basePackages = {"com.dyhc.sdglgroundconnection.mapper"}) // 用于扫描的mapper接口*/
